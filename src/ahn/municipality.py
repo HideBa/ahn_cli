fd7a57b678376e090b85a494ddd61c2f60439460
HideBa <baba.papa1120.ba@gmail.com>
2023-12-21 15:10:31 +0100

    feat: add dutch municipality

---
+++ src/ahn/municipality.py
@@ -0,0 +1,20 @@
+import os
+from geopandas import GeoDataFrame
+import geopandas as gpd
+
+
+def city_polygon(city_name: str) -> GeoDataFrame:
+    """Return a polygon of the city's boundary."""
+    s_city_name = city_name.lower()
+    file_path = os.path.join(
+        os.path.dirname(__file__), "data/municipality_simple.geojson"
+    )
+
+    municipality_df = gpd.read_file(file_path)
+
+    # Filter the DataFrame based on lowercase column values
+    filtered_df = municipality_df[municipality_df["name"].str.lower() == s_city_name]
+    if filtered_df.empty:
+        raise ValueError(f"City {city_name} not found.")
+
+    return filtered_df.iloc[0]["geometry"]
